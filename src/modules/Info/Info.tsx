1e96d571445a0aa9b557b129fa176b3b220a12fe
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-06-16 11:38:11 +0300

    info block created

--- src/modules/Info/Info.tsx
+++ src/modules/Info/Info.tsx
@@ -4,18 +4,20 @@ import React from 'react';
 export const Info: React.FC = () => {
   return (
     <div className="info">
-      <h3 className="info__title main-text">
-        Information about the page
-      </h3>
+      <div className="info__container">
+        <h3 className="info__title main-text d-block text-center">
+          Information about the page
+        </h3>
 
-      <div className="info__content">
-        <h4 className="info__content__title main-text">
-          About the page
-        </h4>
+        <div className="info__content position-relative">
+          <h4 className="info__content__title main-text">
+            About the page
+          </h4>
 
-        <p className="info__desc">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+          <p className="info__content__desc list-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/modules/Select/Select.tsx
+++ src/modules/Select/Select.tsx
@@ -23,206 +23,208 @@ export const Select: React.FC = () => {
   return (
     <>
       <div className="select">
-        <div className={`select--1 celect__content  ${isOpen1 ? 'open' : ''}`}>
-          <div
-            className={`select__header d-flex flex-row justify-content-between ${isOpen1 ? 'select__header--active' : ''}`}
-            role="button"
-            tabIndex={0}
-            onClick={handleToggle1}
-            onKeyDown={handleKeyDown}
-          >
-            <span className="select__option">Specilization</span>
+        <div className="select__container">
+          <div className={`select--1 celect__content  ${isOpen1 ? 'open' : ''}`}>
+            <div
+              className={`select__header d-flex flex-row justify-content-between ${isOpen1 ? 'select__header--active' : ''}`}
+              role="button"
+              tabIndex={0}
+              onClick={handleToggle1}
+              onKeyDown={handleKeyDown}
+            >
+              <span className="select__option">Specilization</span>
 
-            {!isOpen1 ? <img src={arrow} alt="Open" /> : <img src={arrow} alt="Close" className="arrow-close" />}
-          </div>
-          <ul
-            className={`
+              {!isOpen1 ? <img src={arrow} alt="Open" /> : <img src={arrow} alt="Close" className="arrow-close" />}
+            </div>
+            <ul
+              className={`
               select__option__list
               select__option__list--1
               ${isOpen1 ? 'select__option__list--open' : 'select__option__list--close'}`}
-          >
-            <li className="
+            >
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="android-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="android-checkbox"
-                  name="option"
-                  value="Android"
+              >
+                <label htmlFor="android-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="android-checkbox"
+                    name="option"
+                    value="Android"
 
-                />
-                Android
-              </label>
-            </li>
+                  />
+                  Android
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="iOS-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="iOS-checkbox"
-                  name="option"
-                  value="iOS / macOS"
+              >
+                <label htmlFor="iOS-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="iOS-checkbox"
+                    name="option"
+                    value="iOS / macOS"
 
-                />
-                iOS / macOS
-              </label>
-            </li>
+                  />
+                  iOS / macOS
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="C-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="C-checkbox"
-                  name="option"
-                  value="C/C++/Embedded"
+              >
+                <label htmlFor="C-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="C-checkbox"
+                    name="option"
+                    value="C/C++/Embedded"
 
-                />
-                C / C++ / Embedded
-              </label>
-            </li>
+                  />
+                  C / C++ / Embedded
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="Golang-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="Golang-checkbox"
-                  name="option"
-                  value="Golang"
+              >
+                <label htmlFor="Golang-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="Golang-checkbox"
+                    name="option"
+                    value="Golang"
 
-                />
-                Golang
-              </label>
-            </li>
+                  />
+                  Golang
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item
               select__option__list__item--last"
-            >
-              <label htmlFor="Scala-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="Scala-checkbox"
-                  name="option"
-                  value="Scala"
+              >
+                <label htmlFor="Scala-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="Scala-checkbox"
+                    name="option"
+                    value="Scala"
 
-                />
-                Scala
-              </label>
-            </li>
-          </ul>
-        </div>
-
-        <div className={`select--2 celect__content  ${isOpen2 ? 'open' : ''}`}>
-          <div
-            className={`select__header d-flex flex-row justify-content-between ${isOpen2 ? 'select__header--active' : ''}`}
-            role="button"
-            tabIndex={0}
-            onClick={handleToggle2}
-            onKeyDown={handleKeyDown}
-          >
-            <span className="select__option">Seller type</span>
-
-            {!isOpen2 ? <img src={arrow} alt="Open" /> : <img src={arrow} alt="Close" className="arrow-close" />}
+                  />
+                  Scala
+                </label>
+              </li>
+            </ul>
           </div>
 
-          <ul
-            className={`
+          <div className={`select--2 celect__content  ${isOpen2 ? 'open' : ''}`}>
+            <div
+              className={`select__header d-flex flex-row justify-content-between ${isOpen2 ? 'select__header--active' : ''}`}
+              role="button"
+              tabIndex={0}
+              onClick={handleToggle2}
+              onKeyDown={handleKeyDown}
+            >
+              <span className="select__option">Seller type</span>
+
+              {!isOpen2 ? <img src={arrow} alt="Open" /> : <img src={arrow} alt="Close" className="arrow-close" />}
+            </div>
+
+            <ul
+              className={`
               select__option__list
               select__option__list--2
               ${isOpen2 ? 'select__option__list--open' : 'select__option__list--close'}`}
-          >
-            <li className="
+            >
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="first-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="first-checkbox"
-                  name="option"
-                  value="first"
+              >
+                <label htmlFor="first-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="first-checkbox"
+                    name="option"
+                    value="first"
 
-                />
-                first
-              </label>
-            </li>
+                  />
+                  first
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="second-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="second-checkbox"
-                  name="option"
-                  value="second"
+              >
+                <label htmlFor="second-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="second-checkbox"
+                    name="option"
+                    value="second"
 
-                />
-                second
-              </label>
-            </li>
+                  />
+                  second
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="third-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="third-checkbox"
-                  name="option"
-                  value="third"
-                />
-                third
-              </label>
-            </li>
+              >
+                <label htmlFor="third-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="third-checkbox"
+                    name="option"
+                    value="third"
+                  />
+                  third
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item"
-            >
-              <label htmlFor="fourth-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="fourth-checkbox"
-                  name="option"
-                  value="fourth"
+              >
+                <label htmlFor="fourth-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="fourth-checkbox"
+                    name="option"
+                    value="fourth"
 
-                />
-                fourth
-              </label>
-            </li>
+                  />
+                  fourth
+                </label>
+              </li>
 
-            <li className="
+              <li className="
               list-text
               select__option__list__item
               select__option__list__item--last"
-            >
-              <label htmlFor="fifth-checkbox" className="checkbox-label">
-                <input
-                  type="checkbox"
-                  id="fifth-checkbox"
-                  name="option"
-                  value="fifth"
+              >
+                <label htmlFor="fifth-checkbox" className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    id="fifth-checkbox"
+                    name="option"
+                    value="fifth"
 
-                />
-                fifth
-              </label>
-            </li>
-          </ul>
+                  />
+                  fifth
+                </label>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
