5c5ec2b3224fdaf54b1a285d5f1c438b4edeb13b
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-07-19 12:34:32 +0300

    Login block created

--- src/modules/Login/Login.tsx
+++ src/modules/Login/Login.tsx
@@ -1,28 +1,46 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-console */
 import React from 'react';
 
+import profile from '../../img/icons/header/profile.svg';
+import key from '../../img/icons/key.svg';
+
 export const Login: React.FC = () => {
   return (
-    <div className="login d-flex flex-column align-items-center justify-content-center">
-      <div className="login__content">
+    <div className="login position-relative">
+      <div className="login__content position-absolute">
         <h1 className="login__title bold-text d-flex justify-content-center">
           Sign in to continue
         </h1>
 
-        <form action="#" className="login__form d-flex flex-column align-items-center">
-          <input type="text" placeholder="Enter Name or Phone" className="login__form__input login__form__input--1" />
+        <form action="#" className="login__form d-flex flex-column">
+          <div className="login__form__block login__form__block--1 position-relative">
+            <img src={profile} alt="Name or Phone" className="login__form__img login__form__img--profile position-absolute" />
 
-          <input type="text" placeholder="Enter Password" className="login__form__input login__form__input--2" />
+            <input type="text" placeholder="Enter Name or Phone" className="login__form__input" />
+          </div>
 
-          <div className="login__form__bottom d-flex flex-row justify-content-between">
-            <input type="checkbox" className="login__form__bottom__remember" required />
+          <div className="login__form__block login__form__block--2 position-relative">
+            <img src={key} alt="Password" className="login__form__img login__form__img--password position-absolute" />
+
+            <input type="text" placeholder="Enter Password" className="login__form__input login__form__input--2" />
+          </div>
+
+          <div className="login__form__bottom d-flex flex-row justify-content-between align-items-center">
+            <div className="login__form__bottom__remember d-flex flex-row align-items-start">
+              <input type="checkbox" id="rememberMe" className="login__form__bottom__remember__input" />
+
+              <label htmlFor="rememberMe" className="list-text login__form__bottom__remember__label">
+                Remember me
+              </label>
+            </div>
 
             <div className="login__form__bottom__btns d-flex flex-row">
-              <button type="button" className="login__form__bottom__btn login__form__bottom__btn--register">
+              <button type="button" className="login__form__bottom__btn login__form__bottom__btn--register list-text">
                 Register
               </button>
 
-              <button type="button" className="login__form__bottom__btn btn-grey">
+              <button type="button" className="login__form__bottom__btn login__form__bottom__btn--login btn-grey list-text">
                 Login
               </button>
             </div>
