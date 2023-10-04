2f406c6d5d639f650a3e6e8fc9658dad0c6d7270
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-10-04 14:01:44 +0300

    made rating block functional for #rating

--- src/modules/shared/rate/Rate.tsx
+++ src/modules/shared/rate/Rate.tsx
@@ -1,62 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 
 import star from '../../../img/icons/card/rate/star.svg';
+
+import { FaStar } from 'react-icons/fa';
 
 type Props = {
   forRatingBlock?: boolean,
 };
 
 export const Rate: React.FC<Props> = ({ forRatingBlock }) => {
+  const [isRating, setIsRating] = useState(0);
+  const [isColorRate, setIsColorRate] = useState(0);
+
+  if (forRatingBlock) {
+    return (
+      <div className="rate d-flex flex-row align-items-center text-center">
+        {[...Array(5)].map((star, index) => {
+          const currentRate = index + 1;
+          return (
+            <div key={star}>
+              <label>
+                <input
+                  type="radio"
+                  name="rating"
+                  className="d-none rate__input"
+                  value={currentRate}
+                  onClick={() => setIsRating(currentRate)}
+                />
+    
+                <FaStar 
+                  size={23}
+                  className=" rate__star"
+                  color={
+                    currentRate <= (isColorRate || isRating) ? "#F1A449" : "grey"
+                  }
+                  onMouseEnter={() => setIsColorRate(currentRate)}
+                  onMouseLeave={() => setIsColorRate(0)}
+                />
+              </label>
+            </div>
+          );
+        })}
+  
+        <p className='bigger rate__desc small-text'
+        >
+          {`(${isRating}/5)`}
+        </p>
+      </div>
+    );
+  };
+
   return (
     <div className="rate d-flex flex-row align-items-center text-center">
       <div className="rate__stars d-flex flex-row align-items-center">
         <img
           src={star}
           alt="Star"
-          className={classNames(
-            'rate__stars__item',
-            { bigger: forRatingBlock },
-          )}
+          className='rate__stars__item'
         />
         <img
           src={star}
           alt="Star"
-          className={classNames(
-            'rate__stars__item',
-            { bigger: forRatingBlock },
-          )}
+          className='rate__stars__item'
         />
         <img
           src={star}
           alt="Star"
-          className={classNames(
-            'rate__stars__item',
-            { bigger: forRatingBlock },
-          )}
+          className='rate__stars__item'
         />
         <img
           src={star}
           alt="Star"
-          className={classNames(
-            'rate__stars__item',
-            { bigger: forRatingBlock },
-          )}
+          className='rate__stars__item'
         />
         <img
           src={star}
           alt="Star"
-          className={classNames(
-            'rate__stars__item',
-            { bigger: forRatingBlock },
-          )}
+          className='rate__stars__item'
         />
       </div>
 
-      <p className={classNames(
-        'rate__desc small-text',
-        { bigger: forRatingBlock },
-      )}
+      <p className='rate__desc small-text'
       >
         (5/5)
 
